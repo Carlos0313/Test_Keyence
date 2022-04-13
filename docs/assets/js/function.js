60e388502604c4ccf059b7d2850f388dac59eea0
Carlos Najera <carnave.cnv@gmail.com>
2022-04-13 14:47:56 -0500

    sitio web prueba

---
+++ docs/assets/js/function.js
@@ -0,0 +1,49 @@
+var items = [];
+
+const loadData = ()=>{
+    tags = '';
+    contenedor = document.getElementById('tags');
+
+    let data =  localStorage.getItem('items');
+
+    if( data != null){
+        items = JSON.parse(data)
+    }
+
+    items
+    .map(item => {
+        tags += `
+            <div class="tag">
+                <div class="close" onClick="eliminarItem('${item.value}')">x</div>
+                ${item.value}
+            </div>
+        `;
+    })
+
+    contenedor.innerHTML = tags;
+}
+
+const addData = e => items = [...items, {value: e.target.value}]
+
+const pulsar = e => {
+    if (e.keyCode === 13 && !e.shiftKey) {
+
+        let input = document.getElementById('addTag');
+
+        e.preventDefault();
+        addData(e);
+        localStorage.setItem('items', JSON.stringify(items))
+        input.value = '';
+
+        loadData();
+    }
+}
+
+const eliminarItem = value => {
+    items = items.filter(item => ( item.value !== value));
+    localStorage.setItem('items', JSON.stringify(items))
+    loadData();
+}
+
+//Init App
+loadData();
